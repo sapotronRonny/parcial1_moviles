0532043fea72f87ecbc1b289a325e02c8594ab27
salonzo23 <144975523+salonzo23@users.noreply.github.com>
2025-06-07 19:27:28 -0500

    Ajustes V1.0

--- app/src/main/java/com/example/proyecto/Ajustes.kt
+++ app/src/main/java/com/example/proyecto/Ajustes.kt
@@ -1,5 +1,6 @@
 package com.example.proyecto
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
@@ -18,6 +19,16 @@ class Ajustes : AppCompatActivity() {
         }
         findViewById<Button>(R.id.btnOscuro).setOnClickListener {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
+        val btnModificar = findViewById<Button>(R.id.btnModificarPerfil)
+        btnModificar.setOnClickListener {
+            val intent = Intent(this, ActualizarPerfil::class.java)
+            startActivity(intent)
+        }
+        val btnEliminarCuenta = findViewById<Button>(R.id.btnEliminarCuenta)
+        btnEliminarCuenta.setOnClickListener {
+            val intent = Intent(this, EliminarCuenta::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/proyecto/PerfilActivity.kt
+++ app/src/main/java/com/example/proyecto/PerfilActivity.kt
@@ -59,15 +59,5 @@ class PerfilActivity : AppCompatActivity() {
         } else {
             tvPerfil.text = "Usuario no autenticado"
         }
-        val btnModificar = findViewById<Button>(R.id.btnModificarPerfil)
-        btnModificar.setOnClickListener {
-            val intent = Intent(this, ActualizarPerfil::class.java)
-            startActivity(intent)
-        }
-        val btnEliminarCuenta = findViewById<Button>(R.id.btnEliminarCuenta)
-        btnEliminarCuenta.setOnClickListener {
-            val intent = Intent(this, EliminarCuenta::class.java)
-            startActivity(intent)
-        }
     }
 }
